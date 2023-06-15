c83be175e7731ac1e89ff2f1946f3bea7d5737de
aidanangrybird <aidanabgaming@gmail.com>
2023-06-15 10:03:23 -0600

    Fixed geo stelar having unused call for uuid variable that caused crashing

--- skyhighheroes/assets/skyhighheroes/data/heroes/geo_stelar.js
+++ skyhighheroes/assets/skyhighheroes/data/heroes/geo_stelar.js
@@ -375,7 +375,7 @@ function headToggle(player, manager) {
 }
 
 function getAttributeProfile(entity) {
-  if (entity.getData("skyhighheroes:dyn/wave_changing_timer") == 0 && entity.getUUID() == uuid) {
+  if (entity.getData("skyhighheroes:dyn/wave_changing_timer") == 0) {
     if (entity.getData("skyhighheroes:dyn/body_temperature") >= -1.3 && entity.getData("skyhighheroes:dyn/body_temperature") < -0.95) {
       return "FROZEN";
     }
