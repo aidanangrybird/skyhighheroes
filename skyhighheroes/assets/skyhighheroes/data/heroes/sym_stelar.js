9e3ed86f8defbebb96ed4ecc9fc5c1e91c771a30
aidanangrybird <aidanabgaming@gmail.com>
2023-06-23 22:34:04 -0600

    Fixed sym stelars uuid

--- skyhighheroes/assets/skyhighheroes/data/heroes/sym_stelar.js
+++ skyhighheroes/assets/skyhighheroes/data/heroes/sym_stelar.js
@@ -8,7 +8,7 @@ function init(hero) {
   hero.addPrimaryEquipment("fiskheroes:ruptures_scythe{display:{Name:\u00A73Sym Stelar's Scythe},ench:[{id:16,lvl:5},{id:19,lvl:2},{id:20,lvl:2},{id:21,lvl:3},{id:34,lvl:5}]}", true);
   hero.addPrimaryEquipment("fiskheroes:chronos_rifle{display:{Name:\u00A73Sym Stelar's Rifle},ench:[{id:34,lvl:5}]}", true);
   hero.addPrimaryEquipment("fiskheroes:captain_americas_shield{Electromagnetic:1,display:{Name:\u00A73Sym Stelar's Shield},ench:[{id:16,lvl:5},{id:19,lvl:2},{id:20,lvl:2},{id:21,lvl:3},{id:34,lvl:5}]}", true, item => item.nbt().getBoolean("Electromagnetic"));
-  stelar.init(hero, "8d53a071-d3fc-428e-806b-ed16ebe41fe4");
+  stelar.init(hero, "340d3667-9ee9-49eb-b98b-e12c7534b5774");
   hero.setTickHandler((entity, manager) => {
     stelar.tickHandler(entity, manager);
     bodyTemp.change(entity, manager, [
