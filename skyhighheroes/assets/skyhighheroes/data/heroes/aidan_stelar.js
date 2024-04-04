622e039dd8825da24f8a20b47e4ce75a9075b509
aidanangrybird <aidanabgaming@gmail.com>
2024-04-04 01:14:13 -0500

    Changed oc name and am-ian name to be more meteorlogical

--- skyhighheroes/assets/skyhighheroes/data/heroes/aidan_stelar.js
+++ skyhighheroes/assets/skyhighheroes/data/heroes/aidan_stelar.js
@@ -3,5 +3,5 @@ var stelar = implement("skyhighheroes:external/stelar");
 function init(hero) {
   //hero.setName("\u00A76Aidan Stelar");
   hero.setAliases("aidan_stelar");
-  stelar.init(hero, "a3d071d4-c912-41e1-a6b2-c0de99ea4a84", "Aidan Stelar", "Stardust", "Squall Pulsar", "\u00A76");
+  stelar.init(hero, "a3d071d4-c912-41e1-a6b2-c0de99ea4a84", "Aidan Stelar", "Jet-Streak", "Squall Vortex", "\u00A76");
 };
