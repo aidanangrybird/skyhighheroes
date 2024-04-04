4c2b1dfae55f021ffa8ccd8f754a02bd999ccaae
aidanangrybird <aidanabgaming@gmail.com>
2024-04-04 01:15:48 -0500

    removed robins stuff

--- skyhighheroes/assets/skyhighheroes/data/heroes/robin_stelar.js
+++
@@ -1,7 +0,0 @@
-var bodyTemp = implement("skyhighheroes:external/body_temperature");
-var stelar = implement("skyhighheroes:external/stelar");
-function init(hero) {
-  //hero.setName("\u00A72Robin Stelar");
-  hero.setAliases("robin_stelar");
-  stelar.init(hero, "8d53a071-d3fc-428e-806b-ed16ebe41fe4", "Robin Stelar", "Spiega", "Spiega Cosmos", "\u00A72");
-};
