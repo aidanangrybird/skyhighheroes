da38538320eb70441ed6c4470a974cb16959bd8f
aidanangrybird <aidanabgaming@gmail.com>
2024-05-30 14:52:50 -0500

    Added aegon tenma

---
+++ skyhighheroes/assets/skyhighheroes/data/heroes/aegon_tenma.js
@@ -0,0 +1,5 @@
+var tenma = implement("skyhighheroes:external/tenma");
+function init(hero) {
+  hero.setAliases("aegon_tenma");
+  tenma.init(hero, "a3d071d4-c912-41e1-a6b2-c0de99ea4a84", "\u00A7dAegon Tenma");
+}
