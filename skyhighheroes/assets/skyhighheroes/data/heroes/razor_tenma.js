e41f85d03cab914ed96afd587afa53320116e038
aidanangrybird <aidanabgaming@gmail.com>
2024-05-31 14:23:44 -0500

    Added back razor tenma

---
+++ skyhighheroes/assets/skyhighheroes/data/heroes/razor_tenma.js
@@ -0,0 +1,5 @@
+var tenma = implement("skyhighheroes:external/tenma");
+function init(hero) {
+  hero.setAliases("razor_tenma");
+  tenma.init(hero, "de01ddcc-7dc8-437e-bf08-3f8bf11086dc", "\u00A71Razor Tenma");
+}
