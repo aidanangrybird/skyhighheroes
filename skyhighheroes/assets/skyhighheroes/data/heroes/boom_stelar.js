7075c39199ad3070059b86836afd76e20d040c28
aidanangrybird <aidanabgaming@gmail.com>
2024-06-11 12:12:59 -0500

    Removed booms stuff since hes not using it

--- skyhighheroes/assets/skyhighheroes/data/heroes/boom_stelar.js
+++
@@ -1,7 +0,0 @@
-var bodyTemp = implement("skyhighheroes:external/body_temperature");
-var stelar = implement("skyhighheroes:external/stelar");
-function init(hero) {
-  //hero.setName("\u00A75Boom Stelar");
-  hero.setAliases("boom_stelar");
-  stelar.init(hero, "9af1da24-6827-4c48-848d-df07952ff161", "Boom Stelar", "Infinity", "Nova Infinity", "\u00A75");
-};
--- skyhighheroes/assets/skyhighheroes/data/heroes/boom_tenma.js
+++
@@ -1,5 +0,0 @@
-var tenma = implement("skyhighheroes:external/tenma");
-function init(hero) {
-  hero.setAliases("boom_tenma");
-  tenma.init(hero, "9af1da24-6827-4c48-848d-df07952ff161", "\u00A75Boom Tenma");
-}
