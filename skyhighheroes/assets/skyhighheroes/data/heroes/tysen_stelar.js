343c1107a25ab64e3fca42fc0b8fed248ecfbf88
aidanangrybird <aidanabgaming@gmail.com>
2024-06-18 12:38:49 -0500

    Removed tysen stuff

--- skyhighheroes/assets/skyhighheroes/data/heroes/tysen_stelar.js
+++
@@ -1,7 +0,0 @@
-var bodyTemp = implement("skyhighheroes:external/body_temperature");
-var stelar = implement("skyhighheroes:external/stelar");
-function init(hero) {
-  //hero.setName("\u00A75Tysen Stelar");
-  hero.setAliases("tysen_stelar");
-  stelar.init(hero, "0ccdd53f-70bc-4039-b89b-fd3781f746f2", "Tysen Stelar", "Sparkles", "Violet Comet", "\u00A75");
-};
--- skyhighheroes/assets/skyhighheroes/data/heroes/tysen_tenma.js
+++
@@ -1,5 +0,0 @@
-var tenma = implement("skyhighheroes:external/tenma");
-function init(hero) {
-  hero.setAliases("tysen_tenma");
-  tenma.init(hero, "0ccdd53f-70bc-4039-b89b-fd3781f746f2", "\u00A75Tysen Tenma");
-}
