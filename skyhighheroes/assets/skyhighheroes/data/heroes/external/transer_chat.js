7b9cf05ddbe60425714b5333a93099125bb04684
aidanangrybird <aidanabgaming@gmail.com>
2024-09-22 11:47:51 -0500

    Made more progress on transer chat

--- skyhighheroes/assets/skyhighheroes/data/heroes/external/transer_chat.js
+++ skyhighheroes/assets/skyhighheroes/data/heroes/external/transer_chat.js
@@ -1,16 +1,19 @@
 //If I see anyone steal this, I will be very mad as I have spent a lot of time working on this to get it working well
 //So please don't steal this, it will look very bad on you
 
-function isWearingTranser(entities) {
-  entities.forEach(person => {
-    if (person.is("player")) {
-      suits.forEach(entry => {
-        if (person.getWornChestplate().nbt().getString("HeroType") == entry.suit && (typeof entry.id !== "undefined") ? person.getUUID() == entry.id : true) {
-          return true;
-        }
-      })
-    }
+/**
+ * Checks if an entity is wearing a transer
+ * @param {JSEntity} entity - Entity getting checked
+ * @returns If the entity is wearing a transer
+ **/
+function isWearingTranser(entity) {
+  var wearingTranser = false
+  suits.forEach(entry => {
+    if (entity.getWornChestplate().nbt().getString("HeroType") == entry.suit && (typeof entry.id !== "undefined") ? entity.getUUID() == entry.id : true) {
+      wearingTranser = true;
+    };
   });
+  return wearingTranser;
 };
 
 var suits = [
@@ -25,34 +28,65 @@ var suits = [
   {suit: "skyhighheroes:geo_stelar/subaru"}
 ]
 
+/**
+ * Adds contact
+ * @param {JSPlayer} player - Required
+ * @param {JSDataManager} manager - Required
+ * @param {string} uuid - Contact uuid
+ * @param {string} name - Contact name
+ **/
+function addContact(player, manager, uuid, name) {
+  var contacts = player.getWornChestplate().nbt().getTagList("contacts");
+  var contact = manager.newCompoundTag();
+  manager.setString(contact, "name", name);
+  manager.setString(contact, "uuid", uuid);
+  manager.appendTag(contacts, contact);
+};
 
-function addContact(player, manager) {
-  var contacts = player.getWornChestplate().nbt().getTagList("Contacts");
-  var contactNum = contacts.tagCount();
-
+/**
+ * Body beam like how Anit-Monitor has it
+ * @param {JSPlayer} player - Required
+ * @param {JSDataManager} manager - Required
+ * @param contactID - Index of contact
+ * @param name - Name to update contact with
+ **/
+function editContact(player, manager, contactID, name) {
+  var contact = player.getWornChestplate().nbt().getTagList("contacts").getCompoundTag(contactID);
+  manager.setString(contact, "name", name);
 }
 
-function getContacts(player, manager) {
-  var contactList = [];
-  var contacts = player.getWornChestplate().nbt().getTagList("Contacts");
-  var contactNum = contacts.tagCount();
-  for (i = 0; i < contactNum; i++) {
-    var contact = contacts.getCompoundTag(i);
-    contactList.push({"username": contact.getString("username"), "uuid": contact.getString("uuid")});
-  }
-  return contactList;
+/**
+ * Body beam like how Anit-Monitor has it
+ * @param {JSPlayer} player - Required
+ * @param {JSDataManager} manager - Required
+ * @param contactID - Index of contact
+ * @param name - Name to update contact with
+ **/
+function editContact(player, manager, contactID) {
+  var contacts = player.getWornChestplate().nbt().getTagList("contacts");
+  manager.removeTag(contacts, contactID);
 }
 
+/**
+ * Lists player's contacts
+ * @param {JSEntity} entity - Required
+ **/
 function listContacts(entity) {
-  var contacts = entity.getWornChestplate().nbt().getTagList("Contacts");
+  var contacts = entity.getWornChestplate().nbt().getTagList("contacts");
   var contactNum = contacts.tagCount();
   player.addChatMessage("Your saved contacts:");
   for (i = 0; i < contactNum; i++) {
     var contact = contacts.getCompoundTag(i);
-    player.addChatMessage("Username: " + contact.getString("usernames") + "; UUID: " + contact.getString("uuid"));
-  }
-}
+    player.addChatMessage("Username: " + contact.getString("username") + "; UUID: " + contact.getString("uuid"));
+  };
+};
 
+/**
+ * Body beam like how Anit-Monitor has it
+ * @param renderer - This is required or it will not work
+ * @param color - Color to set the beam to
+ * @returns The beam with the color set
+ **/
 function hasContact(self, other) {
   var contacts = listContacts(other);
   for (i=0;i<contacts.length;i++) {
@@ -61,23 +95,41 @@ function hasContact(self, other) {
       return true;
     } else {
       return false;
-    }
-  }
-}
+    };
+  };
+};
 
+/**
+ * Creates group
+ * @param {JSPlayer} player - Required
+ * @param {JSDataManager} manager - Required
+ * @param groupName - Name of group
+ **/
 function createGroup(player, manager, groupName) {
-  var groups = entity.getWornChestplate().nbt().getTagList("Groupss");
-  var groupNum = groups.tagCount();
-  var group = groups.getCompoundTag(groupNum);
-  
-  //Set group name
-}
+  var groups = player.getWornChestplate().nbt().getTagList("groups");
+  var group = manager.newCompoundTag();
+  manager.setString(group, "groupName", groupName);
+  manager.appendTag(groups, group);
+};
+
+/**
+ * Adds member to group
+ * @param {JSPlayer} player - Required
+ * @param {JSDataManager} manager - Required
+ * @param groupID - Index of group to add member to
+ * @param uuid - UUID to add to group
+ **/
+function addGroupMemeber(player, manager, groupID, uuid) {
+  var group = player.getWornChestplate().nbt().getTagList("groups").getCompoundTag(groupID);
+  var members = group.getStringList("members");
+  manager.appendString(members, uuid);
+};
 
 //Make group message function and related stuff
 
 //Add something that will check other players contact list so that message appears under the name the receiving player has set for that uuid
 
-[
+var stuff = [
   {name: "Aidan", uuid: "a3d071d4-c912-41e1-a6b2-c0de99ea4a84"},
   {groupName: "Star Force Guild", groupMembers: [
     "a3d071d4-c912-41e1-a6b2-c0de99ea4a84",
