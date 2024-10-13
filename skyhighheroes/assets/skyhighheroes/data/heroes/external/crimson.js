97e57eb9ac226e4924f2ec784cb3f58d4b2a328b
aidanangrybird <aidanabgaming@gmail.com>
2024-10-13 16:50:34 -0500

    Derped crimson calling conditions, fixed now

--- skyhighheroes/assets/skyhighheroes/data/heroes/external/crimson.js
+++ skyhighheroes/assets/skyhighheroes/data/heroes/external/crimson.js
@@ -137,7 +137,7 @@ function init(transer) {
       return "crimson";
     },
     waveHandler: function (entity, manager) {
-      if (entity.getUUID() == "c4bc5db6-3cf6-44fe-8427-304a7b211bc4" && entity.world().isUnobstructed(entity.pos(), entity.pos().add(0,1000,0)) && entity.posY() >= 105 && entity.world().getLocation(entity.pos()).biome().startsWith("Extreme") && entity.world().isRaining() && entity.world().isThundering()) {
+      if (entity.getUUID() == "c4bc5db6-3cf6-44fe-8427-304a7b211bc4" && entity.world().isUnobstructed(entity.pos(), entity.pos().add(0,1000,0)) && entity.posY() >= 105 && entity.world().getLocation(entity.pos()).biome().startsWith("Extreme") && !entity.world().isRaining() && !entity.world().isThundering()) {
         var value = Math.random();
         if (value < 0.01) {
           manager.setData(entity, "skyhighheroes:dyn/em_being", "Crimson");
