1a8d28ac7e58a3db40cd2bb7d08e3ad475aefa43
aidanangrybird <aidanabgaming@gmail.com>
2024-10-15 14:20:51 -0500

    Removed oc node icons

--- assets/skyhighheroes/renderers/heroes/dragon_transer.js
+++ assets/skyhighheroes/renderers/heroes/dragon_transer.js
@@ -2,6 +2,6 @@ extend("skyhighheroes:base_transer");
 
 loadTextures({
   "transer": "skyhighheroes:dragon_transer",
-  "crimson_asteroid": "skyhighheroes:lucas/lucas_stelar_base",
-  "crimson_asteroid_lights": "skyhighheroes:lucas/lucas_stelar_lights"
+  "crimson_asteroid": "skyhighocs:lucas/lucas_stelar_base",
+  "crimson_asteroid_lights": "skyhighocs:lucas/lucas_stelar_lights"
 });
--- assets/skyhighheroes/renderers/heroes/pegasus_transer.js
+++ assets/skyhighheroes/renderers/heroes/pegasus_transer.js
@@ -4,6 +4,6 @@ loadTextures({
   "transer": "skyhighheroes:pegasus_transer",
   "mega_man": "skyhighheroes:geo/geo_stelar_base",
   "mega_man_lights": "skyhighheroes:geo/geo_stelar_lights",
-  "squall_vortex": "skyhighheroes:aidan/aidan_stelar_base",
-  "squall_vortex_lights": "skyhighheroes:aidan/aidan_stelar_lights",
+  "squall_vortex": "skyhighocs:aidan/aidan_stelar_base",
+  "squall_vortex_lights": "skyhighocs:aidan/aidan_stelar_lights",
 });
