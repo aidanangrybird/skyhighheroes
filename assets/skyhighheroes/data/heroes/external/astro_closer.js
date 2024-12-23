7eaa862ffadca24ade0f0dde628d66716299706b
aidanangrybird <aidanabgaming@gmail.com>
2024-12-23 17:38:55 -0600

    Added more help messages for opener and closer modules

--- assets/skyhighheroes/data/heroes/external/astro_closer.js
+++ assets/skyhighheroes/data/heroes/external/astro_closer.js
@@ -7,7 +7,7 @@ function initModule(system) {
     name: "bodyCloser",
     type: 1,
     command: "close",
-    helpMessage: "<n>!close <nh>-<n> Scanner",
+    helpMessage: "<n>!close <nh>-<n> Panel closer",
     disabledMessage: "<e>Module <eh>bodyCloser<e> is disabled!",
     commandHandler: function (entity, manager, arguments) {
       if (arguments.length > 1 && arguments.length < 4 && entity.getData("skyhighheroes:dyn/astro_clothes") != 3) {
@@ -48,6 +48,20 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/head_bottom_front_open", false);
                 manager.setData(entity, "skyhighheroes:dyn/head_bottom_back_open", false);
                 break;
+              case "help":
+                system.systemMessage(entity, "<n>Body closer commands for <nh>head<n>:");
+                system.systemMessage(entity, "<n>!close head * <nh>-<n> Closes every possible opening for <nh>head<n>");
+                system.systemMessage(entity, "<n>!close head top <nh>-<n> Closes <nh>head<n> top panels");
+                system.systemMessage(entity, "<n>!close head bottom <nh>-<n> Closes <nh>head<n> bottom panels");
+                system.systemMessage(entity, "<n>!close head front <nh>-<n> Closes <nh>head<n> front panels");
+                system.systemMessage(entity, "<n>!close head back <nh>-<n> Closes <nh>head<n> back panels");
+                system.systemMessage(entity, "<n>!close head topFront <nh>-<n> Closes <nh>head<n> front top panel");
+                system.systemMessage(entity, "<n>!close head topBack <nh>-<n> Closes <nh>head<n> back top panel");
+                system.systemMessage(entity, "<n>!close head bottomFront <nh>-<n> Closes <nh>head<n> front bottom panel");
+                system.systemMessage(entity, "<n>!close head bottomBack <nh>-<n> Closes <nh>head<n> back bottom panel");
+                system.systemMessage(entity, "<n>!close head help <nh>-<n> Shows Body closer commands for <nh>head<n>");
+                system.systemMessage(entity, "<n>!close help <nh>-<n> Shows Body closer commands");
+                break;
             };
             break;
           case "body":
@@ -80,7 +94,7 @@ function initModule(system) {
               case "lowerBack":
                 manager.setData(entity, "skyhighheroes:dyn/body_lower_back_open", false);
                 break;
-              case "mGun":
+              case "machineGun":
                 manager.setData(entity, "skyhighheroes:dyn/body_machine_gun_open", false);
                 break;
               case "*":
@@ -90,6 +104,20 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/body_lower_back_open", false);
                 manager.setData(entity, "skyhighheroes:dyn/body_machine_gun_open", false);
                 break;
+              case "help":
+                system.systemMessage(entity, "<n>Body closer commands for <nh>body<n>:");
+                system.systemMessage(entity, "<n>!close body * <nh>-<n> Closes every possible opening for <nh>body<n>");
+                system.systemMessage(entity, "<n>!close body upper <nh>-<n> Closes <nh>body<n> upper panels");
+                system.systemMessage(entity, "<n>!close body lower <nh>-<n> Closes <nh>body<n> lower panels");
+                system.systemMessage(entity, "<n>!close body front <nh>-<n> Closes <nh>body<n> front panels");
+                system.systemMessage(entity, "<n>!close body back <nh>-<n> Closes <nh>body<n> back panels");
+                system.systemMessage(entity, "<n>!close body upperFront <nh>-<n> Closes <nh>body<n> front upper panel");
+                system.systemMessage(entity, "<n>!close body upperBack <nh>-<n> Closes <nh>body<n> back upper panel");
+                system.systemMessage(entity, "<n>!close body lowerFront <nh>-<n> Closes <nh>body<n> front lower panel");
+                system.systemMessage(entity, "<n>!close body lowerBack <nh>-<n> Closes <nh>body<n> back lower panel");
+                system.systemMessage(entity, "<n>!close body machineGun <nh>-<n> Closes <nh>body<n> machine gun");
+                system.systemMessage(entity, "<n>!close body help <nh>-<n> Shows Body closer commands for <nh>body<n>");
+                system.systemMessage(entity, "<n>!close help <nh>-<n> Shows Body closer commands");
             };
             break;
           case "leftArm":
@@ -112,6 +140,16 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/left_arm_cannon_inner_open", false);
                 manager.setData(entity, "skyhighheroes:dyn/left_arm_outer_open", false);
                 break;
+              case "help":
+                system.systemMessage(entity, "<n>Body closer commands for <nh>leftArm<n>:");
+                system.systemMessage(entity, "<n>!close leftArm * <nh>-<n> Closes every possible opening for <nh>leftArm<n>");
+                system.systemMessage(entity, "<n>!close leftArm cannon <nh>-<n> Closes <nh>leftArm<n> cannon panels");
+                system.systemMessage(entity, "<n>!close leftArm cannonOuter <nh>-<n> Closes <nh>leftArm<n> outer cannon panel");
+                system.systemMessage(entity, "<n>!close leftArm cannonInner <nh>-<n> Closes <nh>leftArm<n> inner cannon panel");
+                system.systemMessage(entity, "<n>!close leftArm outer <nh>-<n> Closes <nh>leftArm<n> outer panel");
+                system.systemMessage(entity, "<n>!close leftArm help <nh>-<n> Shows Body closer commands for <nh>leftArm<n>");
+                system.systemMessage(entity, "<n>!close help <nh>-<n> Shows Body closer commands");
+                break;
             };
             break;
           case "rightArm":
@@ -133,6 +171,16 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/right_arm_cannon_outer_open", false);
                 manager.setData(entity, "skyhighheroes:dyn/right_arm_cannon_inner_open", false);
                 manager.setData(entity, "skyhighheroes:dyn/right_arm_outer_open", false);
+                break;
+              case "help":
+                system.systemMessage(entity, "<n>Body closer commands for <nh>rightArm<n>:");
+                system.systemMessage(entity, "<n>!close rightArm * <nh>-<n> Closes every possible opening for <nh>rightArm<n>");
+                system.systemMessage(entity, "<n>!close rightArm cannon <nh>-<n> Closes <nh>rightArm<n> cannon panels");
+                system.systemMessage(entity, "<n>!close rightArm cannonOuter <nh>-<n> Closes <nh>rightArm<n> outer cannon panel");
+                system.systemMessage(entity, "<n>!close rightArm cannonInner <nh>-<n> Closes <nh>rightArm<n> inner cannon panel");
+                system.systemMessage(entity, "<n>!close rightArm outer <nh>-<n> Closes <nh>rightArm<n> outer panel");
+                system.systemMessage(entity, "<n>!close rightArm help <nh>-<n> Shows Body closer commands for <nh>rightArm<n>");
+                system.systemMessage(entity, "<n>!close help <nh>-<n> Shows Body closer commands");
                 break;
             };
             break;
@@ -177,6 +225,21 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/left_leg_boot_back_open", false);
                 manager.setData(entity, "skyhighheroes:dyn/left_leg_boot_open", false);
                 break;
+              case "help":
+                system.systemMessage(entity, "<n>Body closer commands for <nh>leftLeg<n>:");
+                system.systemMessage(entity, "<n>!close leftLeg * <nh>-<n> Closes every possible opening for <nh>leftLeg<n>");
+                system.systemMessage(entity, "<n>!close leftLeg leg <nh>-<n> Closes <nh>leftLeg<n> leg panels");
+                system.systemMessage(entity, "<n>!close leftLeg boot <nh>-<n> Closes <nh>leftLeg<n> boot panels");
+                system.systemMessage(entity, "<n>!close leftLeg front <nh>-<n> Closes <nh>leftLeg<n> front panels");
+                system.systemMessage(entity, "<n>!close leftLeg back <nh>-<n> Closes <nh>leftLeg<n> back panels");
+                system.systemMessage(entity, "<n>!close leftLeg legFront <nh>-<n> Closes <nh>leftLeg<n> front leg panel");
+                system.systemMessage(entity, "<n>!close leftLeg legBack <nh>-<n> Closes <nh>leftLeg<n> back leg panel");
+                system.systemMessage(entity, "<n>!close leftLeg bootFront <nh>-<n> Closes <nh>leftLeg<n> front boot panel");
+                system.systemMessage(entity, "<n>!close leftLeg bootBack <nh>-<n> Closes <nh>leftLeg<n> back boot panel");
+                system.systemMessage(entity, "<n>!close leftLeg bootRocket <nh>-<n> Closes <nh>leftLeg<n> boot rocket");
+                system.systemMessage(entity, "<n>!close leftLeg help <nh>-<n> Shows Body closer commands for <nh>leftLeg<n>");
+                system.systemMessage(entity, "<n>!close help <nh>-<n> Shows Body closer commands");
+                break;
             };
             break;
           case "rightLeg":
@@ -220,6 +283,21 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/right_leg_boot_back_open", false);
                 manager.setData(entity, "skyhighheroes:dyn/right_leg_boot_open", false);
                 break;
+              case "help":
+                system.systemMessage(entity, "<n>Body closer commands for <nh>rightLeg<n>:");
+                system.systemMessage(entity, "<n>!close rightLeg * <nh>-<n> Closes every possible opening for <nh>rightLeg<n>");
+                system.systemMessage(entity, "<n>!close rightLeg leg <nh>-<n> Closes <nh>rightLeg<n> leg panels");
+                system.systemMessage(entity, "<n>!close rightLeg boot <nh>-<n> Closes <nh>rightLeg<n> boot panels");
+                system.systemMessage(entity, "<n>!close rightLeg front <nh>-<n> Closes <nh>rightLeg<n> front panels");
+                system.systemMessage(entity, "<n>!close rightLeg back <nh>-<n> Closes <nh>rightLeg<n> back panels");
+                system.systemMessage(entity, "<n>!close rightLeg legFront <nh>-<n> Closes <nh>rightLeg<n> front leg panel");
+                system.systemMessage(entity, "<n>!close rightLeg legBack <nh>-<n> Closes <nh>rightLeg<n> back leg panel");
+                system.systemMessage(entity, "<n>!close rightLeg bootFront <nh>-<n> Closes <nh>rightLeg<n> front boot panel");
+                system.systemMessage(entity, "<n>!close rightLeg bootBack <nh>-<n> Closes <nh>rightLeg<n> back boot panel");
+                system.systemMessage(entity, "<n>!close rightLeg bootRocket <nh>-<n> Closes <nh>rightLeg<n> boot rocket");
+                system.systemMessage(entity, "<n>!close rightLeg help <nh>-<n> Shows Body closer commands for <nh>rightLeg<n>");
+                system.systemMessage(entity, "<n>!close help <nh>-<n> Shows Body closer commands");
+                break;
             };
             break;
           case "*":
@@ -250,17 +328,18 @@ function initModule(system) {
             manager.setData(entity, "skyhighheroes:dyn/body_machine_gun_open", false);
             break;
           case "help":
-            system.systemMessage(entity, "<n>Body openner commands:");
-            system.systemMessage(entity, "<n>!close <nh><bodyPart><n> <nh>-<n> EEEEEEEEEEE");
-            system.systemMessage(entity, "<n>!close <nh>*<n> <nh>-<n> Opens every possible opening");
-            system.systemMessage(entity, "<n>!close help <nh>-<n> Shows Body openner commands");
+            system.systemMessage(entity, "<n>Body closer commands:");
+            system.systemMessage(entity, "<n>!close <nh><bodyPart><n> * <nh>-<n> Closes every opening on the body part");
+            system.systemMessage(entity, "<n>!close <nh><bodyPart><n> help <nh>-<n> Shows Body closer commands for the body part");
+            system.systemMessage(entity, "<n>!close * <nh>-<n> Closes every possible opening");
+            system.systemMessage(entity, "<n>!close help <nh>-<n> Shows Body closer commands");
             break;
           default:
-            system.systemMessage(entity, "<e>Unknown <eh>bodyOpener<e> command! Try <eh>!close help<e> for a list of commands!");
+            system.systemMessage(entity, "<e>Unknown <eh>bodyCloser<e> command! Try <eh>!close help<e> for a list of commands!");
             break;
         };
       } else {
-        system.systemMessage(entity, "<e>Unknown <eh>bodyOpener<e> command! Try <eh>!close help<e> for a list of commands!");
+        system.systemMessage(entity, "<e>Unknown <eh>bodyCloser<e> command! Try <eh>!close help<e> for a list of commands!");
       };
     },
   };
--- assets/skyhighheroes/data/heroes/external/astro_openner.js
+++ assets/skyhighheroes/data/heroes/external/astro_openner.js
@@ -7,7 +7,7 @@ function initModule(system) {
     name: "bodyOpener",
     type: 1,
     command: "open",
-    helpMessage: "<n>!open <nh>-<n> Scanner",
+    helpMessage: "<n>!open <nh>-<n> Panel opener",
     disabledMessage: "<e>Module <eh>bodyOpener<e> is disabled!",
     commandHandler: function (entity, manager, arguments) {
       if (arguments.length > 1 && arguments.length < 4 && entity.getData("skyhighheroes:dyn/astro_clothes") != 3) {
@@ -47,6 +47,20 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/head_top_back_open", true);
                 manager.setData(entity, "skyhighheroes:dyn/head_bottom_front_open", true);
                 manager.setData(entity, "skyhighheroes:dyn/head_bottom_back_open", true);
+                break;
+              case "help":
+                system.systemMessage(entity, "<n>Body opener commands for <nh>head<n>:");
+                system.systemMessage(entity, "<n>!open head * <nh>-<n> Opens every possible opening for <nh>head<n>");
+                system.systemMessage(entity, "<n>!open head top <nh>-<n> Opens <nh>head<n> top panels");
+                system.systemMessage(entity, "<n>!open head bottom <nh>-<n> Opens <nh>head<n> bottom panels");
+                system.systemMessage(entity, "<n>!open head front <nh>-<n> Opens <nh>head<n> front panels");
+                system.systemMessage(entity, "<n>!open head back <nh>-<n> Opens <nh>head<n> back panels");
+                system.systemMessage(entity, "<n>!open head topFront <nh>-<n> Opens <nh>head<n> front top panel");
+                system.systemMessage(entity, "<n>!open head topBack <nh>-<n> Opens <nh>head<n> back top panel");
+                system.systemMessage(entity, "<n>!open head bottomFront <nh>-<n> Opens <nh>head<n> front bottom panel");
+                system.systemMessage(entity, "<n>!open head bottomBack <nh>-<n> Opens <nh>head<n> back bottom panel");
+                system.systemMessage(entity, "<n>!open head help <nh>-<n> Shows Body opener commands for <nh>head<n>");
+                system.systemMessage(entity, "<n>!open help <nh>-<n> Shows Body opener commands");
                 break;
             };
             break;
@@ -90,6 +104,21 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/body_lower_back_open", true);
                 manager.setData(entity, "skyhighheroes:dyn/body_machine_gun_open", true);
                 break;
+              case "help":
+                system.systemMessage(entity, "<n>Body opener commands for <nh>body<n>:");
+                system.systemMessage(entity, "<n>!open body * <nh>-<n> Opens every possible opening for <nh>body<n>");
+                system.systemMessage(entity, "<n>!open body upper <nh>-<n> Opens <nh>body<n> upper panels");
+                system.systemMessage(entity, "<n>!open body lower <nh>-<n> Opens <nh>body<n> lower panels");
+                system.systemMessage(entity, "<n>!open body front <nh>-<n> Opens <nh>body<n> front panels");
+                system.systemMessage(entity, "<n>!open body back <nh>-<n> Opens <nh>body<n> back panels");
+                system.systemMessage(entity, "<n>!open body upperFront <nh>-<n> Opens <nh>body<n> front upper panel");
+                system.systemMessage(entity, "<n>!open body upperBack <nh>-<n> Opens <nh>body<n> back upper panel");
+                system.systemMessage(entity, "<n>!open body lowerFront <nh>-<n> Opens <nh>body<n> front lower panel");
+                system.systemMessage(entity, "<n>!open body lowerBack <nh>-<n> Opens <nh>body<n> back lower panel");
+                system.systemMessage(entity, "<n>!open body machineGun <nh>-<n> Opens <nh>body<n> machine gun");
+                system.systemMessage(entity, "<n>!open body help <nh>-<n> Shows Body opener commands for <nh>body<n>");
+                system.systemMessage(entity, "<n>!open help <nh>-<n> Shows Body opener commands");
+                break;
             };
             break;
           case "leftArm":
@@ -112,6 +141,16 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/left_arm_cannon_inner_open", true);
                 manager.setData(entity, "skyhighheroes:dyn/left_arm_outer_open", true);
                 break;
+              case "help":
+                system.systemMessage(entity, "<n>Body opener commands for <nh>leftArm<n>:");
+                system.systemMessage(entity, "<n>!open leftArm * <nh>-<n> Opens every possible opening for <nh>leftArm<n>");
+                system.systemMessage(entity, "<n>!open leftArm cannon <nh>-<n> Opens <nh>leftArm<n> cannon panels");
+                system.systemMessage(entity, "<n>!open leftArm cannonOuter <nh>-<n> Opens <nh>leftArm<n> outer cannon panel");
+                system.systemMessage(entity, "<n>!open leftArm cannonInner <nh>-<n> Opens <nh>leftArm<n> inner cannon panel");
+                system.systemMessage(entity, "<n>!open leftArm outer <nh>-<n> Opens <nh>leftArm<n> outer panel");
+                system.systemMessage(entity, "<n>!open leftArm help <nh>-<n> Shows Body opener commands for <nh>leftArm<n>");
+                system.systemMessage(entity, "<n>!open help <nh>-<n> Shows Body opener commands");
+                break;
             };
             break;
           case "rightArm":
@@ -133,6 +172,16 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/right_arm_cannon_outer_open", true);
                 manager.setData(entity, "skyhighheroes:dyn/right_arm_cannon_inner_open", true);
                 manager.setData(entity, "skyhighheroes:dyn/right_arm_outer_open", true);
+                break;
+              case "help":
+                system.systemMessage(entity, "<n>Body opener commands for <nh>rightArm<n>:");
+                system.systemMessage(entity, "<n>!open rightArm * <nh>-<n> Opens every possible opening for <nh>rightArm<n>");
+                system.systemMessage(entity, "<n>!open rightArm cannon <nh>-<n> Opens <nh>rightArm<n> cannon panels");
+                system.systemMessage(entity, "<n>!open rightArm cannonOuter <nh>-<n> Opens <nh>rightArm<n> outer cannon panel");
+                system.systemMessage(entity, "<n>!open rightArm cannonInner <nh>-<n> Opens <nh>rightArm<n> inner cannon panel");
+                system.systemMessage(entity, "<n>!open rightArm outer <nh>-<n> Opens <nh>rightArm<n> outer panel");
+                system.systemMessage(entity, "<n>!open rightArm help <nh>-<n> Shows Body opener commands for <nh>rightArm<n>");
+                system.systemMessage(entity, "<n>!open help <nh>-<n> Shows Body opener commands");
                 break;
             };
             break;
@@ -177,6 +226,21 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/left_leg_boot_back_open", true);
                 manager.setData(entity, "skyhighheroes:dyn/left_leg_boot_open", true);
                 break;
+              case "help":
+                system.systemMessage(entity, "<n>Body opener commands for <nh>leftLeg<n>:");
+                system.systemMessage(entity, "<n>!open leftLeg * <nh>-<n> Opens every possible opening for <nh>leftLeg<n>");
+                system.systemMessage(entity, "<n>!open leftLeg leg <nh>-<n> Opens <nh>leftLeg<n> leg panels");
+                system.systemMessage(entity, "<n>!open leftLeg boot <nh>-<n> Opens <nh>leftLeg<n> boot panels");
+                system.systemMessage(entity, "<n>!open leftLeg front <nh>-<n> Opens <nh>leftLeg<n> front panels");
+                system.systemMessage(entity, "<n>!open leftLeg back <nh>-<n> Opens <nh>leftLeg<n> back panels");
+                system.systemMessage(entity, "<n>!open leftLeg legFront <nh>-<n> Opens <nh>leftLeg<n> front leg panel");
+                system.systemMessage(entity, "<n>!open leftLeg legBack <nh>-<n> Opens <nh>leftLeg<n> back leg panel");
+                system.systemMessage(entity, "<n>!open leftLeg bootFront <nh>-<n> Opens <nh>leftLeg<n> front boot panel");
+                system.systemMessage(entity, "<n>!open leftLeg bootBack <nh>-<n> Opens <nh>leftLeg<n> back boot panel");
+                system.systemMessage(entity, "<n>!open leftLeg bootRocket <nh>-<n> Opens <nh>leftLeg<n> boot rocket");
+                system.systemMessage(entity, "<n>!open leftLeg help <nh>-<n> Shows Body opener commands for <nh>leftLeg<n>");
+                system.systemMessage(entity, "<n>!open help <nh>-<n> Shows Body opener commands");
+                break;
             };
             break;
           case "rightLeg":
@@ -220,6 +284,21 @@ function initModule(system) {
                 manager.setData(entity, "skyhighheroes:dyn/right_leg_boot_back_open", true);
                 manager.setData(entity, "skyhighheroes:dyn/right_leg_boot_open", true);
                 break;
+              case "help":
+                system.systemMessage(entity, "<n>Body opener commands for <nh>rightLeg<n>:");
+                system.systemMessage(entity, "<n>!open rightLeg * <nh>-<n> Opens every possible opening for <nh>rightLeg<n>");
+                system.systemMessage(entity, "<n>!open rightLeg leg <nh>-<n> Opens <nh>rightLeg<n> leg panels");
+                system.systemMessage(entity, "<n>!open rightLeg boot <nh>-<n> Opens <nh>rightLeg<n> boot panels");
+                system.systemMessage(entity, "<n>!open rightLeg front <nh>-<n> Opens <nh>rightLeg<n> front panels");
+                system.systemMessage(entity, "<n>!open rightLeg back <nh>-<n> Opens <nh>rightLeg<n> back panels");
+                system.systemMessage(entity, "<n>!open rightLeg legFront <nh>-<n> Opens <nh>rightLeg<n> front leg panel");
+                system.systemMessage(entity, "<n>!open rightLeg legBack <nh>-<n> Opens <nh>rightLeg<n> back leg panel");
+                system.systemMessage(entity, "<n>!open rightLeg bootFront <nh>-<n> Opens <nh>rightLeg<n> front boot panel");
+                system.systemMessage(entity, "<n>!open rightLeg bootBack <nh>-<n> Opens <nh>rightLeg<n> back boot panel");
+                system.systemMessage(entity, "<n>!open rightLeg bootRocket <nh>-<n> Opens <nh>rightLeg<n> boot rocket");
+                system.systemMessage(entity, "<n>!open rightLeg help <nh>-<n> Shows Body opener commands for <nh>rightLeg<n>");
+                system.systemMessage(entity, "<n>!open help <nh>-<n> Shows Body opener commands");
+                break;
             };
             break;
           case "*":
@@ -250,10 +329,11 @@ function initModule(system) {
             manager.setData(entity, "skyhighheroes:dyn/body_machine_gun_open", true);
             break;
           case "help":
-            system.systemMessage(entity, "<n>Body openner commands:");
-            system.systemMessage(entity, "<n>!open <nh><bodyPart><n> <nh>-<n> EEEEEEEEEEE");
-            system.systemMessage(entity, "<n>!open <nh>*<n> <nh>-<n> Opens every possible opening");
-            system.systemMessage(entity, "<n>!open help <nh>-<n> Shows Body openner commands");
+            system.systemMessage(entity, "<n>Body opener commands:");
+            system.systemMessage(entity, "<n>!open <nh><bodyPart><n> * <nh>-<n> Opens every opening on the body part");
+            system.systemMessage(entity, "<n>!open <nh><bodyPart><n> help <nh>-<n> Shows Body opener commands for the body part");
+            system.systemMessage(entity, "<n>!open * <nh>-<n> Opens every possible opening");
+            system.systemMessage(entity, "<n>!open help <nh>-<n> Shows Body opener commands");
             break;
           default:
             system.systemMessage(entity, "<e>Unknown <eh>bodyOpener<e> command! Try <eh>!open help<e> for a list of commands!");
