26a6b5a57a3ee3bcbd233edb46facd4976354e84
aidanangrybird <aidanabgaming@gmail.com>
2025-03-30 18:04:35 -0500

    Made transers actually say transerOS instead of SYSTEM

--- assets/skyhighheroes/data/heroes/external/transer_system.js
+++ assets/skyhighheroes/data/heroes/external/transer_system.js
@@ -220,13 +220,13 @@ function chatMessage(player, message) {
 function systemMessage(player, message) {
   switch (getAssignedSatellite(player)) {
     case "dragon":
-      chatMessage(player, formatSystem("<d>SYSTEM\u00A7r> " + message));
+      chatMessage(player, formatSystem("<d>transerOS\u00A7r> " + message));
       break;
     case "leo":
-      chatMessage(player, formatSystem("<l>SYSTEM\u00A7r> " + message));
+      chatMessage(player, formatSystem("<l>transerOS\u00A7r> " + message));
       break;
     case "pegasus":
-      chatMessage(player, formatSystem("<p>SYSTEM\u00A7r> " + message));
+      chatMessage(player, formatSystem("<p>transerOS\u00A7r> " + message));
       break;
     default:
       chatMessage(player, formatSystem("<e>Transer is not assigned to a valid satellite!"));
