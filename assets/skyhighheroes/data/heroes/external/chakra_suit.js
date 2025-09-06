c76957becc11bc36e83a2716b7643679da450729
aidanangrybird <aidanabgaming@gmail.com>
2025-09-06 13:59:47 -0500

    Added alias to chakra suits

--- assets/skyhighheroes/data/heroes/external/chakra_suit.js
+++ assets/skyhighheroes/data/heroes/external/chakra_suit.js
@@ -1,6 +1,8 @@
 function initChakras(hero, name) {
   var suit_name = name + " the Invicible";
+  var suit_alias = name.toLowerCase() + "_the_invicible";
   hero.setName(suit_name);
+  hero.setAliases(suit_alias);
   hero.setChestplate("Suit");
 
   hero.addPowers("skyhighheroes:chakra_suit");
